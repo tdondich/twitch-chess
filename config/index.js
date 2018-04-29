495f75cdd97c65d015c08a583b2d552be889a5df
Taylor Dondich <taylor@dondich.com>
2018-04-29 16:12:54 -0700

    Add support for websocket to connect to twitch chat.

--- config/index.js
+++ config/index.js
@@ -45,11 +45,11 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../public/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsRoot: path.resolve(__dirname, '../public'),
+    assetsSubDirectory: 'build',
     assetsPublicPath: '/',
 
     /**
--- server.js
+++ server.js
@@ -1,6 +1,53 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/config/config.json')[env];
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var WebSocketClient = require('websocket').client
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use(express.static('public'))
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+  });
+
+http.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+// Our websocket to twitch.tv
+let webSocket = new WebSocketClient();
+
+webSocket.on('connect', (connection) => {
+    console.log('Twitch Chat Client Connected');
+   connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('close', function() {
+        console.log('echo-protocol Connection Closed');
+    });
+    connection.on('message', function(message) {
+        console.log("Received: '" + message.utf8Data + "'");
+    });
+
+    console.log("Authenticating...");
+
+    connection.send(
+        "CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership"
+    );
+    connection.send("PASS " + config.bot_password);
+    connection.send("NICK " + config.bot_username);
+    connection.send('JOIN #adventuresinprogramming')
+
+
+})
+webSocket.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+webSocket.connect("wss://irc-ws.chat.twitch.tv:443/", null, null, null, null);
+
+
+
+
+
+
